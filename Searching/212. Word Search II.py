5459eb04525b57b5147a068b6c08e91f92d9e426
Ashfak Md Shibli <ams323@njit.edu>
2022-11-05 19:28:04 -0400

    Adding optimized solution to 212. Word Search II

---
+++ Searching/212. Word Search II.py
@@ -0,0 +1,66 @@
+# Problem Statement: https://leetcode.com/problems/word-search-ii/
+
+from typing import List
+
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        WORD_KEY = '$'
+        
+        trie = {}
+        for word in words:
+            node = trie
+            for letter in word:
+                # retrieve the next node; If not found, create a empty node.
+                node = node.setdefault(letter, {})
+                #print("Trie process: ", "for letter", letter , trie)
+
+            # mark the existence of a word in trie node
+            node[WORD_KEY] = word
+            #print("Trie making process after word: ", node)
+        #print("Constructed Trie Dictionary: ", trie)
+        rowNum = len(board)
+        colNum = len(board[0])
+        
+        matchedWords = []
+        
+        def backtracking(row, col, parent):    
+            
+            letter = board[row][col]
+            currNode = parent[letter]
+            #print("parent node:" , parent)
+            #print("current node:" , currNode)
+            # check if we find a match of word
+            word_match = currNode.pop(WORD_KEY, False)
+            if word_match:
+                # also we removed the matched word to avoid duplicates,
+                #   as well as avoiding using set() for results.
+                matchedWords.append(word_match)
+            
+            # Before the EXPLORATION, mark the cell as visited 
+            board[row][col] = '#'
+            
+            # Explore the neighbors in 4 directions, i.e. up, right, down, left
+            for (rowOffset, colOffset) in [(-1, 0), (0, 1), (1, 0), (0, -1)]:
+                newRow, newCol = row + rowOffset, col + colOffset     
+                if newRow < 0 or newRow >= rowNum or newCol < 0 or newCol >= colNum:
+                    continue
+                if not board[newRow][newCol] in currNode:
+                    continue
+                backtracking(newRow, newCol, currNode)
+        
+            # End of EXPLORATION, we restore the cell
+            board[row][col] = letter
+        
+            # Optimization: incrementally remove the matched leaf node in Trie.
+            #print("current node to pop:" , currNode)
+            #print("letter popped: ", letter, "parent: ", parent)
+            if not currNode:
+                parent.pop(letter)
+
+        for row in range(rowNum):
+            for col in range(colNum):
+                # starting from each of the cells
+                if board[row][col] in trie:
+                    backtracking(row, col, trie)
+        
+        return matchedWords    
